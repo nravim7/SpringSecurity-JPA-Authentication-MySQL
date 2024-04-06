5f0f15dbc09353e093e62f3c3c6b16769bb1c619
Ravi N <your.email@example.com>
2024-04-07 01:38:37 +0530

    init setup

---
+++ src/main/java/com/ravi/securityJPAMySql/SecurityJpaMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.securityJPAMySql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJpaMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJpaMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/securityJPAMySql/SecurityJpaMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.securityJPAMySql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJpaMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
