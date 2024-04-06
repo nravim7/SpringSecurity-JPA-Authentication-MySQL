6cef0331ee1096c3900bf04cd9bd702150fe1ffd
Ravi N <your.email@example.com>
2024-04-07 02:31:15 +0530

    JPA connection

--- src/main/java/com/ravi/securityJPAMySql/Models/MyUserDetails.java
+++ src/main/java/com/ravi/securityJPAMySql/Models/MyUserDetails.java
@@ -1,4 +1,4 @@
-package com.ravi.securityJPAMySql;
+package com.ravi.securityJPAMySql.Models;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -6,23 +6,32 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyUserDetails implements UserDetails {
 
     private String userName;
+    private String password;
+    private List<GrantedAuthority> authorities;
 
-    public MyUserDetails(String userName) {
-        this.userName = userName;
+    public MyUserDetails(User user) {
+        this.userName = user.getUserName();
+        this.password = user.getPassword();
+        this.authorities = Arrays.stream(user.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("USER"));
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "pass";
+        return password;
     }
 
     @Override
--- src/main/java/com/ravi/securityJPAMySql/MyUserDetailsService.java
+++ src/main/java/com/ravi/securityJPAMySql/MyUserDetailsService.java
@@ -1,15 +1,27 @@
 package com.ravi.securityJPAMySql;
 
+import com.ravi.securityJPAMySql.Models.MyUserDetails;
+import com.ravi.securityJPAMySql.Models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new MyUserDetails(username);
+        Optional<User> user = userRepository.findByUserName(username);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found " + username));
+
+        return user.map(MyUserDetails::new).get();
     }
 }
